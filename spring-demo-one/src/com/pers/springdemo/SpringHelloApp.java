482266fa7c9bf83326d08562cb8cf931ad5b1f16
stacywang0601 <stacywang61@gmail.com>
2017-05-30 13:34:55 -0400

    configure spring using xml file

---
+++ spring-demo-one/src/com/pers/springdemo/SpringHelloApp.java
@@ -0,0 +1,20 @@
+package com.pers.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringHelloApp {
+	public static void main(String[] args) {
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		// retrieve bean from the container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+
+		// call method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+
+		// close the context
+		context.close();
+
+	}
+}
