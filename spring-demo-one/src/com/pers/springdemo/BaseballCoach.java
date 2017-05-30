92cfd1f5a4db8ede92cc4155d12a4e145adf4f8d
stacywang0601 <stacywang61@gmail.com>
2017-05-30 13:56:49 -0400

    dependency injection:2. Create a constructor for dependency injection and use it

--- spring-demo-one/src/com/pers/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/pers/springdemo/BaseballCoach.java
@@ -1,6 +1,14 @@
 package com.pers.springdemo;
 
 public class BaseballCoach implements Coach {
+	// define a private field for dependency
+	private FortuneService	fortuneService;
+
+	// define a constructor for dependency injection
+	public BaseballCoach(FortuneService thefortuneService) {
+		fortuneService = thefortuneService;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
@@ -8,7 +16,7 @@ public class BaseballCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		// use my fortuneService to get a fortune
+		return fortuneService.getFortune();
 	}
 }
