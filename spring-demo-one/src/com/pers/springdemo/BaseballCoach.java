7efc4a90d888b6a404af30e68f7955e223dc4937
stacywang0601 <stacywang61@gmail.com>
2017-05-30 13:14:21 -0400

    baseballcoach

---
+++ spring-demo-one/src/com/pers/springdemo/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.pers.springdemo;
+
+public class BaseballCoach {
+
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting practice";
+	}
+}
---
+++ spring-demo-one/src/com/pers/springdemo/MyApp.java
@@ -0,0 +1,11 @@
+package com.pers.springdemo;
+
+public class MyApp {
+	public static void main(String[] args) {
+		// create the object
+		BaseballCoach theCoach = new BaseballCoach();
+		// use the object
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
