82ba05e23c73cedfef3d73cb6c05627fa2946396
stacywang0601 <stacywang61@gmail.com>
2017-06-01 15:29:54 -0400

    create setter method for literal value

--- spring-demo-one/src/com/pers/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/pers/springdemo/CricketCoach.java
@@ -4,6 +4,10 @@ public class CricketCoach implements Coach {
 
 	private FortuneService	fortuneService;
 
+	// add new fields for emailAddress and team
+	private String	       emailAddress;
+	private String	       team;
+
 	// create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
@@ -23,6 +27,24 @@ public class CricketCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
+	}
+
+	public String getTeam() {
+		return team;
 	}
 
 }
