2d73583f4896eaec72f716ab3d904f7b0c7ea445
stacywang0601 <stacywang61@gmail.com>
2017-06-01 15:03:10 -0400

    try dependency injection in trackCoach

--- spring-demo-one/src/com/pers/springdemo/SpringHelloApp.java
+++ spring-demo-one/src/com/pers/springdemo/SpringHelloApp.java
@@ -13,6 +13,9 @@ public class SpringHelloApp {
 		// call method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 
+		// let's call our new method for fortunes
+		System.out.println(theCoach.getDailyFortune());
+
 		// close the context
 		context.close();
 
