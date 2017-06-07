80dfd04adb4daed37d12e6aa0c1d57c714f489ba
stacywang0601 <stacywang61@gmail.com>
2017-06-07 16:12:59 -0400

    annotation test bean scope

--- spring-demo-annotation/src/com/pers/springdemo/TennisCoach.java
+++ spring-demo-annotation/src/com/pers/springdemo/TennisCoach.java
@@ -2,10 +2,12 @@ package com.pers.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 // bean id
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 	@Autowired
 	@Qualifier("randomFortuneService")
