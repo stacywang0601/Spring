cf6e5826161a7f1362add682e82dec30b4c88fda
stacywang0601 <stacywang61@gmail.com>
2017-06-07 15:39:24 -0400

    annotation setter injection default constructor setter method autowired

--- spring-demo-annotation/src/com/pers/springdemo/TennisCoach.java
+++ spring-demo-annotation/src/com/pers/springdemo/TennisCoach.java
@@ -8,11 +8,24 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
 	private FortuneService	fortuneService;
 
-	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
 	}
 
+	// define a setter method
+	@Autowired
+	public void setFortuneService(FortuneService thefortuneService) {
+		System.out.println(">> TennisCoach: inside setter method");
+		fortuneService = thefortuneService;
+	}
+
+	/*
+	 * @Autowired
+	 * public TennisCoach(FortuneService theFortuneService) {
+	 * fortuneService = theFortuneService;
+	 * }
+	 */
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
