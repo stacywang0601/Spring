accc83361dce68a6aae40239d9413c3d06135736
stacywang0601 <stacywang61@gmail.com>
2017-06-08 12:10:09 -0400

    config spring using xml file

---
+++ spring-xml/src/com/pers/springdemo/MyApp.java
@@ -0,0 +1,11 @@
+package com.pers.springdemo;
+
+public class MyApp {
+	public static void main(String[] args) {
+		// create the object
+		Coach theCoach = new TrackCoach();
+		// use the object
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
